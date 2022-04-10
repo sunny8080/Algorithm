b952ac6e95fb4fe2dc516b31fac267afa8b41d4b
Sunny <sunny.kumar.it23@heritgeit.edu.in>
2022-04-11 00:47:59 +0530

    Rename some files

--- 27.1_Policy_Based_Data_Str.cpp
+++ 27.1_Policy_Based_Data_Str.cpp
@@ -1,111 +1,72 @@
 #include<bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
-// #include<functional> // for less<>
-
 using namespace std;
 using namespace __gnu_pbds;
 
-// Policy Based Data Structure (PBDS)
+#define ll 				 long long
+#define ff 				 first
+#define ss 				 second
+#define pb 				 push_back
+#define mp 				 make_pair
+#define pii 			 pair<int,int>
+#define vi 				 vector<int>
+#define vll 			 vector<long long> 
+#define vvi 			 vector< vector<int>>
+#define vvll 			 vector< vector<long long>>
+#define mii 			 map<int,int>
+#define pqb 			 priority_queue<int>
+#define pqs 			 priority_queue<int, vector<int>, greater<int>>
+#define setbits(x)		 __builtin_popcountll(x)
+#define zrobits(x) 		 __builtin_ctzll(x)
+#define mod 			 1000000009
+#define inf 			 1e18
+#define ps(x,y) 		 fixed<<setprecision(y)<<x
+#define mk(arr, n, type) type *arr=new type[n];
+#define wt(x) 			 int x; cin>>x; while( x-- )
+#define rep(i,a,b) 		 for( int i=a; i<=b; i++ )
+#define repi(i,a,b) 	 for( int i=a; i>=b; i-- )
+#define sp 				 ' '
+#define nl 				 char(10)
+#define endl 			 char(10)
+#define PRT(ar) 		 for( auto i : ar )cout<<i<<sp;cout<<nl;
+#define mem(x,ch) 		 memset(x,ch,sizeof(x))
+#define sortv(x) 		 sort(x.begin(),x.end())
+#define sortvr(x) 		 sort(x.rbegin(),x.rend())
 
-// template is used for more specific
-template<class T>
-using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const ll cnst = 1e6 + 5;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+typedef tree< pair<ll,ll>, null_type, less<pair<ll,ll>>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+void fastIO(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    // #ifndef ONLINE_JUDGE
+	// 	freopen("input.txt", "r", stdin);
+	// 	freopen("output.txt", "w", stdout) ;
+    // #endif
+}
+
+
 
 int32_t main(){
-
-    // For set or Ordered set
-
-    oset<int> st;
-    st.insert(10);
-    st.insert(20);
-    st.insert(3);
-    st.insert(5);
-    st.insert(45);
-    st.insert(9);
-    st.insert(9); // doubles not allowed
+    fastIO();
     
-    // find_by_order() : kth largest element till now
-    for( int i=0; i<st.size(); i++ ){
-        cout<<i<<" "<< *st.find_by_order(i)<< "\n";
+    ll q, k; cin>>q>>k;
+    pbds dist;
+    priority_queue<ll> pq;
+    while(q--){
+    	ll type; cin>>type;
+    	if( type == 1 ){
+    		ll x, y; cin>>x>>y;
+    		x = pow(x,2) + pow(y,2);
+    		pq.push(x);
+            if( pq.size() > k ) pq.pop();
+    	}else{
+    		cout<<pq.top()<<nl;
+    	}
     }
 
-    // 0 3
-    // 1 5
-    // 2 9
-    // 3 10
-    // 4 20
-    // 5 45
 
-    // order_of_key(n) : return no. of items strictly less than n // as st is less PBDS
-    cout<<st.order_of_key(10)<<endl; // 3
-    cout<<st.order_of_key(13)<<endl; // 4
-    cout<<endl<<endl;
-
-
-
-    // For pair or Map
-    oset<pair<int,int>> pst;
-    // {value, key} or {value, index} // index or key should be unique
-    pst.insert({5, 0});
-    pst.insert({10, 4}); 
-    pst.insert({3, 3}); 
-    pst.insert({35, 15}); 
-    pst.insert({35, 10}); 
-    pst.insert({35, 20}); 
-    pst.insert({64, 7});
-    pst.insert(make_pair(80, 10)); 
-
-    // value at ith index
-    cout<<"Contains of map : "<<endl;
-    for( auto i=0; i<pst.size(); i++ ){
-        auto pr = pst.find_by_order(i);
-        cout<<i<<" : ( "<<pr->first<<", "<<pr->second<<" )"<<endl;
-    }
-    cout<<endl;
-
-    // Contains of map :
-    // 0 : ( 3, 3 )
-    // 1 : ( 5, 0 )
-    // 2 : ( 10, 4 )
-    // 3 : ( 35, 10 )
-    // 4 : ( 35, 15 )
-    // 5 : ( 35, 20 )
-    // 6 : ( 64, 7 )
-    // 7 : ( 80, 10 )
-
-    // index of pair(64, 7 ) // pair present
-    cout<<"Index of pair(64,7) : "<< pst.order_of_key({64,7})<<endl; // 6
-
-    // index of pair(64, 10 ) // pair not present
-    cout<<"Index of pair(64, 10) : "<< pst.order_of_key({64, 10})<<endl; // 7
-
-    // index of pair(40, 5) // pair not present
-    cout<<"Index of pair(40, 5) : "<< pst.order_of_key({40, 5})<<endl; // 6
-
-    
     return 0;
 }
 
-// 0 3
-// 1 5
-// 2 9
-// 3 10
-// 4 20
-// 5 45
-// 3
-// 4
-
-// Contains of map :
-// 0 : ( 3, 3 )
-// 1 : ( 5, 0 )
-// 2 : ( 10, 4 )
-// 3 : ( 35, 10 )
-// 4 : ( 35, 15 )
-// 5 : ( 35, 20 )
-// 6 : ( 64, 7 )
-// 7 : ( 80, 10 )
-
-// Index of pair(64,7) : 6
-// Index of pair(64, 10) : 7
-// Index of pair(40, 5) : 6
--- 33.2_Linear_Reccurance_Matrix_Exponentiation.cpp
+++ 33.2_Linear_Reccurance_Matrix_Exponentiation.cpp
@@ -19,7 +19,7 @@ using namespace __gnu_pbds;
 #define pqs              priority_queue<int, vector<int>, greater<int>>
 #define setbits(x)       __builtin_popcountll(x)
 #define zrobits(x)       __builtin_ctzll(x)
-#define mod              1000000000
+#define mod              1000000009
 #define inf              1e18
 #define ps(x,y)          fixed<<setprecision(y)<<x
 #define mk(arr, n, type) type *arr=new type[n];
@@ -39,53 +39,56 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 typedef tree< int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 
 void fastIO(){
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 //    #ifndef ONLINE_JUDGE
 //        freopen("input.txt", "r", stdin);
 //        freopen("output.txt", "w", stdout);
 //    #endif
 }
 
+
+// Question Discussed here
 // Linear Reccurance Matrix Exponentiation
-// Q -> https://www.spoj.com/problems/SEQ/ 
+// https://www.spoj.com/problems/SPP/
+
 
 vll b,c;
-ll k;
+ll k, MOD ;
 
 class Solution {
-    
-    vector<vector<ll>> multiply( vector<vector<ll>> A, vector<vector<ll>> B ){
+	vector<vector<ll>> multiply( vector<vector<ll>> A, vector<vector<ll>> B, ll size ){
         
-        vector<vector<ll>> C(k+1, vector<ll>(k+1) );
+        vector<vector<ll>> C(size+1, vector<ll>(size+1) );
         // Multiply two vectors
-        for( int i=1; i<=k; i++ ){
-            for( int j=1; j<=k; j++ ){
-                for( int t=1; t<=k; t++ ){
-                    C[i][j] = ( C[i][j] + (A[i][t]*B[t][j])%mod)%mod;
+        for( int i=1; i<=size; i++ ){
+            for( int j=1; j<=size; j++ ){
+                for( int t=1; t<=size; t++ ){
+                    C[i][j] = ( C[i][j] + (A[i][t]*B[t][j])%MOD)%MOD;
                 }
             }
         }
         return C;
     }
 
-    vector<vector<ll>> pwr( vector<vector<ll>> M, int n ){
+    vector<vector<ll>> pwr( vector<vector<ll>> M, ll n, ll size ){
         if( n == 1 ) return M;
         if( n&1 ){
-            return multiply(M,pwr(M, n-1));
+            return multiply(M,pwr(M, n-1, size), size);
         }
-        vector<vector<ll>> tmp = pwr(M,n/2);
-        return multiply(tmp, tmp);
+        vector<vector<ll>> tmp = pwr(M,n/2, size);
+        return multiply(tmp, tmp, size);
     }
 
-    public:
-    ll solve(ll n ){
+	// Not require for this Question
+	// Previous Q :- https://www.spoj.com/problems/SEQ/
+	ll findAn(ll n ){
         if( n == 0 ) return 0;
         if( n <= k ) return b[n-1];
 
-        // array to store f(n) :- F1(n) 
+        // array to store f(n) - F1(n) 
         // 1 based indexing
         vll F1(k+1);
-        for( int i=1; i<=k; i++ ) F1[i] = b[i-1];
+        for( ll i=1; i<=k; i++ ) F1[i] = b[i-1];
 
         // create Transformation Matrix
         vector<vector<ll> > T(k+1, vector<ll>(k+1) );
@@ -105,54 +108,108 @@ class Solution {
         }
 
         // Step - 3 // T -> T^n
-        T = pwr(T, n-1 );
+        T = pwr(T, n-1, k );
 
         // step - 4 // Matrix[1][i] * vector[i]
         ll ans = 0;
         for( int i=1; i<=k; i++ ){
-            ans = (ans + (T[1][i]*F1[i]) %mod )%mod;
+            ans = (ans + (T[1][i]*F1[i]) %MOD )%MOD;
         }
         return ans;
     }
+
+	ll sum( ll n ){
+		if( n == 0 ) return 0;
+		if( n <= k ) return accumulate(b.begin(), b.begin()+n, 0);
+
+		// array 1 based Indexing
+		vll F1(k+2);
+		F1[1] = 1;
+		for( int i=1; i<=k+1; i++ ){
+			if( i == 1 ){
+				F1[i] = accumulate(b.begin(), b.end(), 0);
+			}else{
+				F1[i] = b[i-2];
+			}
+		}
+		
+		// create Transformation matrix
+		vector<vector<ll>> T(k+2, vector<ll>(k+2));
+		
+		for( ll i=1; i<=k+1; i++ ){
+			for( ll j=1; j<=k+1; j++ ){
+				if( i == 1 ){
+					if( j == 1 ) T[i][j] = 1;
+					else T[i][j] = c[k-j+1];
+				}else if( i == k+1 ){
+					if( j == 1 ) T[i][j] = 0;
+					else T[i][j] = c[k-j+1];
+				}else{
+					if( j == i+1 ) T[i][j] = 1;
+					else T[i][j] = 0;
+				}
+			}
+		}
+
+		// for(auto x : T) { PRT(x); } // Transformation Matrix
+
+		// step -3
+		T = pwr(T, n-k, k+1);
+
+		// for(auto x : T) { PRT(x); }
+
+		// step-4
+		ll sum=0;
+		for( ll i=1; i<=k+1; i++ ){
+			sum = ( sum + (T[1][i]*F1[i]) % MOD) %MOD;
+		}
+
+
+		return sum;
+	}
+
+
+	public:
+
+	ll solve( ll m, ll n ){
+		ll cnt1 = sum(m-1);
+		ll cnt2 = sum(n);
+
+		// cout<<"sum(m-1) : "<<cnt1<<nl<<nl;
+		// cout<<"sum(n) : "<<cnt2<<nl;
+
+		ll ans = ( ( cnt2 - cnt1 )%MOD + MOD ) % MOD ;
+		return ans;
+	}
 };
 
 int32_t main(){
-    fastIO();
-    
-    wt(t){
-        cin>>k;
-        ll x; 
-        for( int i=0; i<k; i++ ) { cin>>x; b.push_back(x); }; 
-        for( int i=0; i<k; i++ ) { cin>>x; c.push_back(x); };
-        ll n; cin>>n;
+	fastIO();
+	
 
-        cout<<(new Solution())->solve(n)<<nl;
+	wt(t){
+		cin>>k;
+		ll x;
+		for( ll i=0; i<k; i++ ){ cin>>x; b.pb(x); }
+		for( ll i=0; i<k; i++ ){ cin>>x; c.pb(x); }
+		ll m, n; 
+		cin>>m>>n>>MOD;
 
-        b.clear();
-        c.clear();
-    }
-    
-    
-    return 0;
+		cout<<(new Solution())->solve(m ,n)<<nl;
+
+		b.clear();
+		c.clear();
+	}
+
+	
+	return 0;
 }
-    
-
-
-// 3 
-// 3 
-// 5 8 2 
-// 32 54 6 
-// 2 
-// 3 
-// 1 2 3 
-// 4 5 6 
-// 6 
-// 3 
-// 24 354 6 
-// 56 57 465 4
-// 98765432
-//
-// OUTPUT:-
-// 8
-// 714
-// 257599514
+	
+	
+// 1
+// 2
+// 1 1
+// 1 1
+// 2 10 1000003
+// output :- 
+// 142
