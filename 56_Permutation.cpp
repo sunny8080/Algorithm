dc16d917fec9f01ce9dd03b6e5a0ba4d2cf5acb8
Sunny <sunny.kumar.it23@heritgeit.edu.in>
2022-04-10 11:21:41 +0530

    Create 56

---
+++ 56_Permutation.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+
+#define ll               long long
+#define ull              unsigned long long
+#define ff               first
+#define ss               second
+#define pb               push_back
+#define mp               make_pair
+#define pii              pair<int,int>
+#define vi               vector<int>
+#define vll              vector<ll> 
+#define vvi              vector< vector<int>>
+#define vvll             vector< vector<ll>>
+#define mii              map<int,int>
+#define pqb              priority_queue<int>
+#define pqs              priority_queue<int, vector<int>, greater<int>>
+#define setbits(x)       __builtin_popcountll(x)
+#define zrobits(x)       __builtin_ctzll(x)
+#define mod              1000000007
+#define inf              1e18
+#define ps(x,y)          fixed<<setprecision(y)<<x
+#define mk(arr, n, type) type *arr=new type[n];
+#define wt(x)            int x; cin>>x; while( x-- )
+#define rep(i,a,b)       for( int i=a; i<=b; i++ )
+#define repi(i,a,b)      for( int i=a; i>=b; i-- )
+#define sp               ' '
+#define nl               char(10)
+#define endl             char(10)
+#define PRT(ar)          for( auto i : ar ) cout<<i<<sp; cout<<nl;
+#define mems(x,ch)       memset(x,ch,sizeof(x))
+#define sortv(x)         sort(x.begin(),x.end())
+#define sortvr(x)        sort(x.rbegin(),x.rend())
+#define all(x)           x.begin(), x.end()
+
+const ll cnst = 1e5 + 5;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// typedef tree< int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+void fastIO(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+//    #ifndef ONLINE_JUDGE
+//        freopen("input.txt", "r", stdin);
+//        freopen("output.txt", "w", stdout);
+//    #endif
+}
+
+class Solution {
+    
+    public:
+    void permute( char *in, int i){
+        if( ! in[i] ){
+            cout<<in<<sp;
+            return;
+        }
+
+        // rec case
+        for( int j=i; in[j]!='\0'; j++ ){
+            swap(in[i], in[j]);
+            permute(in, i+1);
+
+            // Backtracking
+            swap(in[j], in[i]);
+        }
+    }
+};
+
+
+int32_t main(){
+    fastIO();
+    
+    Solution sol;
+
+    // char in[] = { "132" };
+    char in[] = { "123" };
+    sol.permute(in, 0); // 123 132 213 231 321 312
+    
+    return 0;
+}
+    
+    
